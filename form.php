a0511c426ee3c3532cb6731deea1d7f25e11df87
Ismail Hossen <ihossen016@gmail.com>
2022-09-21 12:41:39 +0600

    task form created

--- form.php
+++ form.php
@@ -1,8 +1,9 @@
 <?php
 
-
+// Load Form Style
 add_action('wp_enqueue_scripts', 'load_assets');
 
+// Add stylesheet function
 function load_assets(){
     wp_enqueue_style(
         'contact-form-style',
@@ -11,4 +12,25 @@ function load_assets(){
         1,
         'all'
     );
+}
+
+// Load task form function
+function load_task_form(){
+    return ('
+    <form class="task-form">
+        <h3 class="task-form-title">Create New Task</h3>
+        <div class="form-body">
+            <div class="form-group">
+                <label class="input-label" for="task-title">Title</label>
+                <input type="text" name="task-title" placeholder="Enter Task Title" required/>
+            </div>
+            
+            <div class="form-group">
+                <label class="input-label" for="task-description">Description</label>
+                <textarea type="text" rows="5" name="task-description" placeholder="Enter Task Description" required></textarea>
+            </div>
+        </div>
+        <button type="submit" class="task-form-btn">Create Task</button>
+    </form>
+    ');
 }
